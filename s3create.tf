f331aa65e5bbbe2046fd9a50f8c755c72b729176
krishna538 <30768989+krishna538@users.noreply.github.com>
2020-01-11 19:18:29 +0530

    Update s3create.tf

--- s3create.tf
+++ s3create.tf
@@ -1,5 +1,12 @@
+provider "aws" {
+  region = "ap-south-1"
+  access_key = "AKIAXENY7ZOYCLZBJ6EI"
+  secret_key = "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"
+
+}
+
 resource "aws_s3_bucket" "b" {
-  bucket = "my-tf-test-bucket"
+  bucket = "my-tf-test235-bucket"
   acl    = "private"
 
   tags = {
